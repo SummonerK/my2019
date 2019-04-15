c49e31e1d9ceca98d48d289e6ac42132650cbe76
Lofi <fei.luo@freemud.cn>
2019-04-15 17:54:55 +0800

    ball

--- myRepository/myRepository/ViewController.swift
+++ myRepository/myRepository/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view_cake: UIView!
+    @IBOutlet weak var viewHeight: NSLayoutConstraint!
+    var isOpen:Bool = true
+    @IBOutlet weak var bton_cake: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeCake(_ sender: Any) {
+        bton_cake.isEnabled = false
+        
+//        UIView.animate(withDuration: 0.8, delay: 0, options: .curveEaseInOut, animations: {
+//            self.view_cake.layer.cornerRadius = self.isOpen ? 0 : self.view_cake.frame.height/2
+//        }) { (complete) in
+//            self.isOpen = !self.isOpen
+//            self.bton_cake.isEnabled = true
+//        }
+        
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.view_cake.layer.cornerRadius = self.isOpen ? 0 : self.view_cake.frame.height/2
+        }) { (complete) in
+            self.isOpen = !self.isOpen
+            self.bton_cake.isEnabled = true
+        }
+    }
+    
 }
 
